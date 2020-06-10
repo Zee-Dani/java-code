aa3a1b45736c2b5cdd95ae5770d723dedbc1b8e0
Zidani Zipya <zidzipya@gmail.com>
2020-06-10 17:28:04 +0100

    square&cube

---
+++ Squares.java
@@ -0,0 +1,43 @@
+public class Squares{
+	public static void main(String... args){
+	
+	int a = 0;
+	int b = 1;
+	int c = 2;
+	int d = 3;
+	int e = 4; 
+	int f = 5;
+	int g = 6;
+	int h = 7;
+	int i = 8;
+	int j = 9;
+	int k = 10;
+
+	System.out.println("Numbers\tSquares\tCube");
+	System.out.println(a+"\t"+a*a+"\t"+a*a*a);
+	
+	System.out.println(b+"\t"+b*b+"\t"+b*b*b);
+	System.out.println(c+"\t"+c*c+"\t"+c*c*c);
+	System.out.println(d+"\t"+d*d+"\t"+d*d*d);
+	System.out.println(e+"\t"+e*e+"\t"+e*e*e);
+	System.out.println(f+"\t"+f*f+"\t"+f*f*f);
+	System.out.println(g+"\t"+g*g+"\t"+g*g*g);
+	System.out.println(h+"\t"+h*h+"\t"+h*h*h);
+	System.out.println(i+"\t"+i*i+"\t"+i*i*i);
+	System.out.println(j+"\t"+j*j+"\t"+j*j*j);
+	System.out.println(k+"\t"+k*k+"\t"+k*k*k);
+
+	
+}
+}
+
+
+
+
+
+
+
+
+
+
+
