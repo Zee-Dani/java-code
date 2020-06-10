5e146fb27b97a9dadd95847103a92d049ba171cc
Zidani Zipya <zidzipya@gmail.com>
2020-06-10 17:48:36 +0100

    asterisks

---
+++ Asterisks.java
@@ -0,0 +1,51 @@
+public class Asterisks{
+	public static void main(String...args){
+	System.out.println("* * * * * * * *");
+	System.out.println(" * * * * * * * *");
+	System.out.println("* * * * * * * *");
+	System.out.println(" * * * * * * * *");
+	System.out.println("* * * * * * * *");
+	System.out.println(" * * * * * * * *");
+	System.out.println("* * * * * * * *");
+	System.out.println(" * * * * * * * *");
+	
+	System.out.printf( "%s%n%s%n", "This is a java", "Program");
+
+
+	System.out.printf(  "%s%n%s%n" ,"this is a java", "Program" );
+
+
+	System.out.printf("*%n**%n***%n****%n*****%n");
+
+	
+	System.out.println("*");
+System.out.println("***");
+System.out.println("*****");
+System.out.println("****");
+System.out.println("**");
+
+
+System.out.print("*");
+System.out.print("***");
+System.out.print("*****");
+System.out.print("****");
+System.out.println("**");
+
+		System.out.print("*");
+		System.out.println("***");
+		System.out.println("*****");
+		System.out.print("****");
+		System.out.println("**");
+
+
+
+		System.out.printf("%s%n%s%n%s%n", "*", "***", "*****");
+		
+
+
+
+}
+
+
+
+}
