a9952e334d0b592e8170e3d4123ddbf6d44d7068
Zidani Zipya <zidzipya@gmail.com>
2020-06-11 02:13:19 +0100

    testing java operands

---
+++ Chaptwo.java
@@ -0,0 +1,40 @@
+public class Chaptwo{
+
+	public static void main(String[]  args){
+
+	int x, y,z,v ,a ;
+	a=1;
+	x = 2 ;
+	y = 3 ;
+	System.out.printf("x=%d%n",x);	
+
+	System.out.printf("Value of %d +%d is %d%n", x, x,(x+x));
+
+	System.out.printf("x = ");
+	
+	
+	System.out.printf("%d = %d%n" ,(x + y),(y+x));
+	
+	z= a* x* x* x* + 7;
+
+
+	v= a* (x* x* x) + 7;
+
+	System.out.println ();
+
+
+	System.out.println("1 ,2 ,3, 4");
+	System.out.print("1");
+	System.out.print(" 2");
+	System.out.print(" 3");
+	System.out.print(" 4");
+	System.out.println ();
+
+	System.out.printf("%d %d %d %d", 1,2,3,4);
+
+
+	
+	
+	
+}
+}
